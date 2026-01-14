2ad6ae949afe6f926ff9e00c6a1f65bd48bd6903
maeleffendi <fido_a86@hotmail.com>
2026-01-14 21:07:23 +0000

    Update max6675_proc.vhd

--- SPI_MAX6675/max6675_proc.vhd
+++ SPI_MAX6675/max6675_proc.vhd
@@ -1,87 +1,84 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-entity max6675_proc is
-		port (
-			sys_clk		: in std_logic;
-			reset			: in std_logic;
-			Drdy			: in std_logic;
-			Din			: in std_logic_vector(15 downto 0);
-			Dout			: out std_logic_vector (7 downto 0);
-			Drdyo			: out std_logic;
-			led1			: out std_logic;
-			led2			: out	std_logic;
-			led3			: out std_logic
-		
-		);
+ENTITY max6675_proc IS
+	PORT (
+		sys_clk : IN std_logic;
+		reset : IN std_logic;
+		Drdy : IN std_logic;
+		Din : IN std_logic_vector(15 DOWNTO 0);
+		Dout : OUT std_logic_vector (7 DOWNTO 0);
+		Drdyo : OUT std_logic;
+		led1 : OUT std_logic;
+		led2 : OUT std_logic;
+		led3 : OUT std_logic 
+	);
 
-end max6675_proc;
+END max6675_proc;
 
-architecture behav of max6675_proc is
+ARCHITECTURE behav OF max6675_proc IS
 
-	signal led1_reg	: std_logic;
-	signal led2_reg	: std_logic;
-	signal led3_reg	: std_logic;
-	signal Din_reg		: std_logic_vector(11 downto 0);
-	signal Drdy_reg	: std_logic;
-	signal Dout_reg	: std_logic_vector (15 downto 0);
+	SIGNAL led1_reg : std_logic;
+	SIGNAL led2_reg : std_logic;
+	SIGNAL led3_reg : std_logic;
+	SIGNAL Din_reg : std_logic_vector(11 DOWNTO 0);
+	SIGNAL Drdy_reg : std_logic;
+	SIGNAL Dout_reg : std_logic_vector (15 DOWNTO 0);
 
-begin
-
-	process (sys_clk, reset) is
-	
-	variable temp_meas	: integer;
-	
-	begin
-	
-			if (reset = '1') then
+BEGIN
+	PROCESS (sys_clk, reset) IS
+ 
+	VARIABLE temp_meas : INTEGER;
+ 
+	BEGIN
+		IF (reset = '1') THEN
+			led1_reg <= '0';
+			led2_reg <= '0';
+			led3_reg <= '0';
+			Din_reg <= (OTHERS => '0');
+			Dout_reg <= (OTHERS => '0');
+			Drdy_reg <= '0';
+ 
+ 
+		ELSIF (rising_edge(sys_clk)) THEN
+			IF (Drdy = '1') THEN
+				Din_reg <= Din(14 DOWNTO 3);
+			END IF;
+ 
+			Drdy_reg <= Drdy;
+			temp_meas := to_integer(unsigned(Din_reg)) * 250;
+			Dout_reg <= std_logic_vector(to_unsigned(temp_meas, 16) SRL 8);
+ 
+			IF (temp_meas < 15000) THEN
 				led1_reg <= '0';
 				led2_reg <= '0';
 				led3_reg <= '0';
-				Din_reg <= (others => '0');
-				Dout_reg <= (others => '0');
-				Drdy_reg <= '0';
-				
-				
-			elsif (rising_edge(sys_clk)) then
-				if (Drdy = '1') then
-					Din_reg <= Din(14 downto 3);
-				end if;
-				
-				Drdy_reg <= Drdy;
-				temp_meas := to_integer(unsigned(Din_reg)) * 250;
-				Dout_reg <= std_logic_vector(to_unsigned(temp_meas,16) srl 8);
-				
-					if (temp_meas < 15000) then
-						led1_reg <= '0';
-						led2_reg <= '0';
-						led3_reg <= '0';
-					elsif (temp_meas >= 15000 and temp_meas < 20000) then
-						led1_reg <= '1';
-						led2_reg <= '0';
-						led3_reg <= '0';
-					elsif (temp_meas >= 20000 and temp_meas < 35000) then
-						led1_reg <= '0';
-						led2_reg <= '1';
-						led3_reg <= '0';
-					elsif (temp_meas >= 35000) then
-						led1_reg <= '0';
-						led2_reg <= '0';
-						led3_reg <= '1';
-					else 
-						led1_reg <= '1';
-						led2_reg <= '1';
-						led3_reg <= '1';
-					end if;
-				
-			
-			end if;
-	end process;
-	led1 <= not led1_reg;
-	led2 <= not led2_reg;
-	led3 <= not led3_reg;
+			ELSIF (temp_meas >= 15000 AND temp_meas < 20000) THEN
+				led1_reg <= '1';
+				led2_reg <= '0';
+				led3_reg <= '0';
+			ELSIF (temp_meas >= 20000 AND temp_meas < 35000) THEN
+				led1_reg <= '0';
+				led2_reg <= '1';
+				led3_reg <= '0';
+			ELSIF (temp_meas >= 35000) THEN
+				led1_reg <= '0';
+				led2_reg <= '0';
+				led3_reg <= '1';
+			ELSE
+				led1_reg <= '1';
+				led2_reg <= '1';
+				led3_reg <= '1';
+			END IF;
+ 
+ 
+		END IF;
+	END PROCESS;
+	led1 <= NOT led1_reg;
+	led2 <= NOT led2_reg;
+	led3 <= NOT led3_reg;
 	Drdyo <= Drdy_reg;
-	Dout <= Dout_reg(7 downto 0);
+	Dout <= Dout_reg(7 DOWNTO 0);
 
-end behav;
+END behav;
